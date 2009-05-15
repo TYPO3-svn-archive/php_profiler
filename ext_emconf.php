70ec6cccfcb5b4a020acf46f7f748369a0670932
kj187 <kj187@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-15 21:00:53 +0000

    Commit emconf after TER upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/php_profiler/tags/php_profiler_example@20373 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "php_profiler_example"
 #
-# Auto generated 14-05-2009 23:34
+# Auto generated 15-05-2009 22:59
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -12,7 +12,7 @@
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'PHP Quick Profiler Example',
-	'description' => 'Example for EXT: php_profiler',
+	'description' => 'Example Extension for EXT: php_profiler',
 	'category' => 'plugin',
 	'author' => 'Julian Kleinhans',
 	'author_email' => 'typo3@kj187.de',
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.0.1',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"ab0c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"807c";s:17:"ext_localconf.php";s:4:"4830";s:14:"ext_tables.php";s:4:"4fee";s:16:"locallang_db.xml";s:4:"a274";s:19:"doc/wizard_form.dat";s:4:"9fc0";s:20:"doc/wizard_form.html";s:4:"88a9";s:39:"pi1/class.tx_phpprofilerexample_pi1.php";s:4:"0530";s:17:"pi1/locallang.xml";s:4:"4a07";}',
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"ab0c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"807c";s:17:"ext_localconf.php";s:4:"4830";s:14:"ext_tables.php";s:4:"5225";s:16:"locallang_db.xml";s:4:"a274";s:19:"doc/wizard_form.dat";s:4:"9fc0";s:20:"doc/wizard_form.html";s:4:"88a9";s:39:"pi1/class.tx_phpprofilerexample_pi1.php";s:4:"6dd9";s:17:"pi1/locallang.xml";s:4:"4a07";}',
 	'suggests' => array(
 	),
 );
